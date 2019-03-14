4a4596471e92beb68b7da2afbe79e750dbef2a65
Stefan Scepanovic <scepa91@icloud.com>
2019-03-14 12:17:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function dwarfRollCall(dwarves) {
+  return ("1. Dopey 2. Grumpy 3. Bashful ");
+
+  var dwarves = ["Dopey", "Grumpy", "Bashful"];
 }
 
+function summonCaptainPlanet(veggies){
+  var veggies = ["carrot", "cucumber", "pepper"];
+ console.log(veggies.lenght);
+
+}
+function summonCaptainPlanet(){
+  var fruits = ["apple", "banana", "orange"];
+  var result = summonCaptainPlanet(fruits);
+summonCaptainPlanet(fruits[1]) = "APPLE!";
+summonCaptainPlanet(fruits[2]) = "BANANA!";
+summonCaptainPlanet(fruits[3]) = "ORANGE!";
+}
+
+
+
 function summonCaptainPlanet(planeteerCalls){
+planeteerCalls.toUppercase();
+var earth = "EARTH!";
+var wind = "WIND!";
+var fire = "FIRE!";
+var water = "WATER!";
+var heart = "HEART!"
+  planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+  return summonCaptainPlanet(["EARTH!", "WIND!", "FIRE!", "WATER!", "HEART!"]);
 }
 
 function longPlaneteerCalls(words) {
 }
 
-function findTheCheese (foods) {
+function findTheCheese(cheddarCheese) {
+  var cheddarCheese = ["banana", "cheddar", "sock"];
+  console.log(findTheCheese[1]);
+}
+
+function noCheeseArray(noCheeseArray){
+  var noCheeseArray = ["banana", "tomato", "sock"];
+  console.log('no cheese!');
 }
